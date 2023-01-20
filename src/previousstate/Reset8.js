60badb7675410d2a15913b702d521f3ed0dd706b
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-01-20 16:33:23 +0300

    reset previous state

---
+++ src/previousstate/Reset8.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export default function Reset8() {
+  const initialCount = 80;
+  const [count, setCount] = useState(initialCount);
+
+  const incrementFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount((prevCount) => prevCount + 10);
+    }
+  };
+
+  return (
+    <div>
+      Count:{count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount((prevCount) => prevCount + 10)}>
+        Increment
+      </button>
+      <button onClick={() => setCount((prevCount) => prevCount - 10)}>
+        decrement
+      </button>
+      <button onClick={incrementFive}>Increment 5</button>
+    </div>
+  );
+}
