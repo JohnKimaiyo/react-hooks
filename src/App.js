3c59b3143533425beb1859d0d67d6648eba0d8dc
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-01-20 09:45:58 +0300

    count 30+

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Count1 from "./usestate/Count1";
 import Count2 from "./usestate/Count2";
-
+import Count3 from "./usestate/Count3";
 function App() {
   return <div className="App">
     <Count1/>
     <Count2/>
+    <Count3/>
   </div>;
 }
 
